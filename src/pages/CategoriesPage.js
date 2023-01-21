59fe4676060d3f6dc21e0cba449da5e6c87503c0
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-21 00:49:43 -0500

    Add Categories component.

---
+++ src/pages/CategoriesPage.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class CategoriesPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render = () => (
+    <div>
+      <button type="button">CHECK STATUS</button>
+    </div>
+  );
+}
+export default CategoriesPage;
