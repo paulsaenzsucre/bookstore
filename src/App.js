fab6fe8b7e9bc8838fbfef39d55607fc6b419a2a
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-21 01:32:08 -0500

    Testing Header component rendering.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Header />
     </div>
   );
 }
