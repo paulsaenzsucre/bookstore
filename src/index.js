01e9cb71c9f54ea22e1d1ecbd3ba5c5499b1fd3c
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-21 01:54:28 -0500

    Delete bolierplate styles and unused code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
