d25bd3ef8dc04643658b936dc1cb31135917bb7d
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-21 00:10:09 -0500

    Testing Booklist rendering.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Book from './components/Book';
+import Booklist from './components/Booklist';
 
 function App() {
   return (
     <div className="App">
-      <Book
-        category="Action"
-        title="The Hunger Games"
-        author="Suzanne Collins"
-        chapters={25}
-        currentChapter={16}
-      />
+      <Booklist />
     </div>
   );
 }
