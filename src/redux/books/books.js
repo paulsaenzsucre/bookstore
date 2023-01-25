fc37977d482584037b5060d4dc60a01d8f71af24
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-25 16:49:12 -0500

    Set book's reducer default state to some books.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,31 @@ const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 // Reducer
-const reducer = (state = [], action = {}) => {
+const reducer = (state = [{
+  id: 0,
+  category: 'Action',
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+  chapters: 25,
+  currentChapter: 16,
+},
+{
+  id: 1,
+  category: 'Science Fiction',
+  title: 'Dune',
+  author: 'Frank Herbert',
+  chapters: 25,
+  currentChapter: 2,
+},
+{
+  id: 2,
+  category: 'Economy',
+  title: 'Capital in the Twenty-First century',
+  author: 'Suzanne Collins',
+  chapters: 30,
+  currentChapter: 0,
+},
+], action = {}) => {
   let newState;
   switch (action.type) {
     case ADD_BOOK:
