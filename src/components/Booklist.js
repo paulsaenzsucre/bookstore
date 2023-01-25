faac61e1d23d247659a0a0523d0abfb559513cc6
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-25 17:11:52 -0500

    Refactor Booklist to function-based and get state from redux store.

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,54 +1,24 @@
-import { Component } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-class Booklist extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [{
-        id: 0,
-        category: 'Action',
-        title: 'The Hunger Games',
-        author: 'Suzanne Collins',
-        chapters: 25,
-        currentChapter: 16,
-      },
-      {
-        id: 1,
-        category: 'Science Fiction',
-        title: 'Dune',
-        author: 'Frank Herbert',
-        chapters: 25,
-        currentChapter: 2,
-      },
-      {
-        id: 2,
-        category: 'Economy',
-        title: 'Capital in the Twenty-First century',
-        author: 'Suzanne Collins',
-        chapters: 30,
-        currentChapter: 0,
-      },
-      ],
-    };
-  }
+const Booklist = () => {
+  const books = useSelector((state) => state.books);
 
-  render = () => {
-    const { books } = this.state;
-    return (
-      <div>
-        {books.map((book) => (
-          <Book
-            key={book.id}
-            category={book.category}
-            title={book.title}
-            author={book.author}
-            chapters={book.chapters}
-            currentChapter={book.currentChapter}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {books.map((book) => (
+        <Book
+          key={book.id}
+          category={book.category}
+          title={book.title}
+          author={book.author}
+          chapters={book.chapters}
+          currentChapter={book.currentChapter}
+        />
+      ))}
+    </div>
+  );
+};
+
 export default Booklist;
