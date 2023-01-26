1fa3da04b2509a0a910479570288a5c2dba80b32
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-26 12:28:19 -0500

    Setup redux-thunk on Bookstore.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categoriesreducer from './categories/categories';
 
@@ -7,6 +8,7 @@ const store = configureStore({
     books: booksReducer,
     categories: categoriesreducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export default store;
